5df950e540e63ae1dc8a3aad678612223425467f
Roman Khabirov <khabirovroman@gmail.com>
2015-03-05 16:08:14 +0600

    Final commit

--- question/api.py
+++ question/api.py
@@ -18,24 +18,18 @@ class PostResource(ModelResource):
   title = fields.CharField(attribute="title", use_in="list")
   text = fields.CharField(attribute="text", use_in="list")
   is_public = fields.CharField(attribute="is_public", use_in="detail")
+  comments = fields.ToManyField("question.api.CommentResource", full=True, null=True, 
+      attribute=lambda bundle: Comment.objects.filter(post=bundle.obj), use_in='detail')
   class Meta:
     queryset = Post.objects.all()
     resource_name = 'post'
     authorization = Authorization()
     always_return_data = True
 
-  # commentList = fields.ListField(attribute='commentList', use_in="list", readonly=True)
-  # def dehydrate(self, bundle):
-  #   cmt_res = CommentResource()
-  #   cmt_bundles = [cmt_res.build_bundle(i) for i in bundle.obj.commentList]
-  #   for cb in cmt_bundles:
-  #     cmt_res.full_dehydrate(cb)
-  #   bundle.data['commentList'] = cmb_bundles
-
 class CommentResource(ModelResource):
   author = fields.CharField(attribute="author", use_in="list")
   text = fields.CharField(attribute="text", use_in="list")
-  post = fields.OneToOneField(PostResource, 'post')
+  post = fields.ToOneField(PostResource, 'post')
   class Meta:
     queryset = Comment.objects.all()
     resource_name = 'comment'
